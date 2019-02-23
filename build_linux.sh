b8747c55647921f325fb7d525a11a88fe0347355
Pierce <piercerod57@gmail.com>
2019-02-23 12:42:21 -0700

    linux script 2.0erino

--- build_linux.sh
+++ build_linux.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-compiler_flags=   -I../source/
-linker_flags=     -lX11
-executable_name=  fight_linux
+compiler_flags="-I ../source/"
+linker_flags="-lX11"
+executable_name="fight_linux"
 
 if [ ! -d "build" ]; then
   mkdir build
